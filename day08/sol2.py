f3a23e7fb380a8b2f46f5f788282600ed9cf83f0
Massimo Santini <santini@di.unimi.it>
2021-12-08 12:27:59 +0100

    Replace a for with index

--- day08/sol2.py
+++ day08/sol2.py
@@ -4,18 +4,17 @@ SEGMENTS = 'abcdefg'
 DIGITS = 'abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg'
 
 PERMS = []
+TRANS = []
 for p in permutations(SEGMENTS):
   t = str.maketrans(SEGMENTS, ''.join(p))
-  f = str.maketrans(''.join(p), SEGMENTS)
-  PERMS.append((f, frozenset(''.join(sorted(d.translate(t))) for d in DIGITS)))
+  PERMS.append(frozenset(''.join(sorted(d.translate(t))) for d in DIGITS))
+  TRANS.append(str.maketrans(''.join(p), SEGMENTS))
 
 n = 0
 with open('input1.txt') as inf:
   for line in inf:
     l, r = line.strip().split('|')
-    d = frozenset(''.join(sorted(_)) for _ in l.split())
-    for f, pd in PERMS:
-      if pd == d:
-        n += int(''.join(map(str, (DIGITS.index(''.join(sorted(o.translate(f)))) for o in r.split()))))
-        break
+    d = frozenset(''.join(sorted(i)) for i in l.split())
+    f = TRANS[PERMS.index(d)]
+    n += int(''.join(map(str, (DIGITS.index(''.join(sorted(o.translate(f)))) for o in r.split()))))
 print(n)
