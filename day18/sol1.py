d1ba62964562adc994c73465dd9328eae13cfa29
Massimo Santini <santini@di.unimi.it>
2021-12-19 23:55:04 +0100

    Two

--- day18/sol1.py
+++ day18/sol1.py
@@ -1,51 +1,77 @@
-def split(snailfish):
+def explode(snailfish):
 
   pos2node = dict()
   cnt = 0
-  split = None
+  explode = None
   parent = None
   def add_depth(num, depth = 0):
-    nonlocal cnt, split, parent
+    nonlocal cnt, explode, parent
     l, r = num
+    le = None
     if isinstance(l, list):
+      le = cnt
       add_depth(l, depth + 1)
     else:
       cnt += 1
       pos2node[cnt] = 'l', num
     if isinstance(r, list):
+      if le is None: le = cnt
       add_depth(r, depth + 1)
     else:
       cnt += 1
       pos2node[cnt] = 'r', num
     if parent is None and depth == 3 and (isinstance(l, list) or isinstance(r, list)):
       parent = num
-      split = cnt - 1, cnt
+      explode = le + 1, le + 2
 
   add_depth(snailfish)
 
-  if parent:
-    l, r = parent
-    if isinstance(l, int):
-      ta = parent[1]
-      parent[1] = 0
-    if isinstance(r, int):
-      ta = parent[0]
-      parent[0] = 0
+  if parent is None: return False
+  l, r = parent
+  if isinstance(l, list):
+    ta = parent[0]
+    parent[0] = 0
+  elif isinstance(r, list):
+    ta = parent[1]
+    parent[1] = 0
 
-    ls, rs = split
-    if ls - 1 in pos2node:
-      w, ln = pos2node[ls - 1]
-      ln[0 if w == 'l' else 1] += ta[0]
-    if rs + 1 in pos2node:
-      w, rn = pos2node[rs + 1]
-      rn[0 if w == 'l' else 1] += ta[1]
+  ls, rs = explode
+  if ls - 1 in pos2node:
+    w, ln = pos2node[ls - 1]
+    ln[0 if w == 'l' else 1] += ta[0]
+  if rs + 1 in pos2node:
+    w, rn = pos2node[rs + 1]
+    rn[0 if w == 'l' else 1] += ta[1]
 
+  return True
 
-num = [[[[[9,8],1],2],3],4]
-num = [7,[6,[5,[4,[3,2]]]]]
-num = [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
-num = [[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]
-num = [[6,[5,[4,[3,2]]]],1]
+def split(snailfish):
+  split = False
+
+  def _split(num):
+    nonlocal split
+    if split or isinstance(num, int): return
+    l, r = num
+    if isinstance(l, list):
+      _split(l)
+    elif not split and l >= 10:
+      split = True
+      num[0] = [l // 2, l - (l // 2)]
+    if isinstance(r, list):
+      _split(r)
+    elif not split and r >= 10:
+      split = True
+      num[1] = [r // 2, r - (r // 2)]
+
+  _split(snailfish)
+  return split
+
+num = [[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]], [7,[[[3,7],[4,3]],[[6,3],[8,8]]]]]
+
 print(num)
-split(num)
+
+while True:
+  if explode(num): continue
+  if not split(num): break
+
 print(num)
