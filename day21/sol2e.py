ec5670c317dc9aa5385a9875f02718d47554cb45
Massimo Santini <santini@di.unimi.it>
2021-12-21 22:07:02 +0100

    Now works

--- day21/sol2e.py
+++ day21/sol2e.py
@@ -1,16 +1,14 @@
 from itertools import product
 from collections import defaultdict, namedtuple
 
-sum2cnt = defaultdict(int)
+dice2cnt = defaultdict(int)
 for i, j, k in product(range(1, 4), range(1, 4), range(1, 4)):
-  sum2cnt[i + j + k] += 1
-sum2cnt = dict(sum2cnt)
-sum2cnt = {3:1, 2:1}
-print(sum2cnt)
+  dice2cnt[i + j + k] += 1
+dice2cnt = dict(dice2cnt)
 
 Universe = namedtuple('Universe', 'pos, pts')
 
-U = {Universe((3, 7), (0, 0)): 1}
+U = {Universe((4, 8), (0, 0)): 1}
 
 turn = 0
 wins = [0, 0]
@@ -18,10 +16,10 @@ wins = [0, 0]
 while U:
   Up = defaultdict(int)
   for u, uc in U.items():
-    for d, dc in sum2cnt.items():
+    for d, dc in dice2cnt.items():
       npos = list(u.pos)
       npts = list(u.pts)
-      npos[turn] = (npos[turn] + d) % 9
+      npos[turn] = (npos[turn] + d) % 10
       npts[turn] += npos[turn] + 1
       Up[Universe(tuple(npos), tuple(npts))] += uc * dc
   U = defaultdict()
@@ -30,8 +28,6 @@ while U:
       wins[turn] += uc
     else:
       U[u] = uc
-  for u, uc in U.items(): print(uc, (u.pos[0], u.pts[0]), (u.pos[1], u.pts[1]))
   turn = 1 - turn
-  print(wins)
 
-print(max(wins) / 444356092776315)
+print(max(wins))
