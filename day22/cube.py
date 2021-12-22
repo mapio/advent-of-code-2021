52c4424fdd4ac88c554baddd4059a63135b8c928
Massimo Santini <santini@di.unimi.it>
2021-12-22 23:41:20 +0100

    Now using cuboids

--- day22/cube.py
+++ day22/cube.py
@@ -5,12 +5,12 @@ def pairwise(iterable):
     next(b, None)
     return zip(a, b)
 
-class Rectangle:
+class Cuboid:
 
-    __slots__ = '__x1', '__y1', '__x2', '__y2'
+    __slots__ = '__x1', '__y1', '__z1', '__x2', '__y2', '__z2'
 
-    def __init__(self, x1, y1, x2, y2):
-        self.__setstate__((min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)))
+    def __init__(self, x1, y1, z1, x2, y2, z2):
+        self.__setstate__((min(x1, x2), min(y1, y2), min(z1, z2), max(x1, x2), max(y1, y2), max(z1, z2)))
 
     def __repr__(self):
         return '{}({})'.format(type(self).__name__, ', '.join(map(repr, self)))
@@ -25,7 +25,7 @@ class Rectangle:
         return hash(self.data)
 
     def __len__(self):
-        return 4
+        return 6
 
     def __getitem__(self, key):
         return self.data[key]
@@ -34,36 +34,41 @@ class Rectangle:
         return iter(self.data)
 
     def __and__(self, other):
-        x1, y1, x2, y2 = max(self.x1, other.x1), max(self.y1, other.y1), \
-                         min(self.x2, other.x2), min(self.y2, other.y2)
-        if x1 < x2 and y1 < y2:
-            return type(self)(x1, y1, x2, y2)
+        x1, y1, z1, x2, y2, z2 = max(self.x1, other.x1), max(self.y1, other.y1), max(self.z1, other.z1), \
+                                 min(self.x2, other.x2), min(self.y2, other.y2), min(self.z2, other.z2)
+        if x1 < x2 and y1 < y2 and z1 < z2:
+            return type(self)(x1, y1, z1, x2, y2, z2)
 
     def __sub__(self, other):
         intersection = self & other
         if intersection is None:
             yield self
         else:
-            x, y = {self.x1, self.x2}, {self.y1, self.y2}
+            x, y, z = {self.x1, self.x2}, {self.y1, self.y2}, {self.z1, self.z2}
             if self.x1 < other.x1 < self.x2:
                 x.add(other.x1)
             if self.y1 < other.y1 < self.y2:
                 y.add(other.y1)
+            if self.z1 < other.z1 < self.z2:
+                z.add(other.z1)
             if self.x1 < other.x2 < self.x2:
                 x.add(other.x2)
             if self.y1 < other.y2 < self.y2:
                 y.add(other.y2)
-            for (x1, x2), (y1, y2) in product(pairwise(sorted(x)),
-                                              pairwise(sorted(y))):
-                instance = type(self)(x1, y1, x2, y2)
+            if self.z1 < other.z2 < self.z2:
+                z.add(other.z2)
+            for (x1, x2), (y1, y2), (z1, z2) in product(pairwise(sorted(x)),
+                                                        pairwise(sorted(y)),
+                                                        pairwise(sorted(z))):
+                instance = type(self)(x1, y1, z1, x2, y2, z2)
                 if instance != intersection:
                     yield instance
 
     def __getstate__(self):
-        return self.x1, self.y1, self.x2, self.y2
+        return self.x1, self.y1, self.z1, self.x2, self.y2, self.z2
 
     def __setstate__(self, state):
-        self.__x1, self.__y1, self.__x2, self.__y2 = state
+        self.__x1, self.__y1, self.__z1, self.__x2, self.__y2, self.__z2 = state
 
     @property
     def x1(self):
@@ -74,12 +79,20 @@ class Rectangle:
         return self.__y1
 
     @property
+    def z1(self):
+        return self.__z1
+
+    @property
     def x2(self):
         return self.__x2
 
     @property
     def y2(self):
         return self.__y2
+
+    @property
+    def z2(self):
+        return self.__z2
 
     @property
     def width(self):
@@ -90,8 +103,12 @@ class Rectangle:
         return self.y2 - self.y1
 
     @property
-    def area(self):
-        return self.width * self.height
+    def depth(self):
+        return self.z2 - self.z1
+
+    @property
+    def volume(self):
+        return self.width * self.height * self.depth
 
     intersection = __and__
 
@@ -100,9 +117,9 @@ class Rectangle:
     data = property(__getstate__)
 
 if __name__ == '__main__':
-    A = Rectangle(0 - .5, 0 - .5, 2 + .5, 2 + .5)
-    print(A, A.area)
-    B = Rectangle(1 - 0.5, 1 -0.5, 3 + .5, 3 + .5)
-    print(B, B.area)
-    print('A - B', list(A-B), sum(r.area for r in A - B))
-    print('B - A', list(B-A), sum(r.area for r in B - A))
+    A = Cuboid(0, 0, 0, 2, 2, 2)
+    print(A, A.volume)
+    B = Cuboid(1, 1, 1, 3, 3, 3)
+    print(B, B.volume)
+    print('A - B', list(A-B), sum(r.volume for r in A - B))
+    print('B - A', list(B-A), sum(r.volume for r in B - A))
--- day22/sol2.py
+++ day22/sol2.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
 from sol1 import read
-from rect import Rectangle
+from cube import Cuboid
 from tqdm import tqdm, trange
 
 def add(D, s):
@@ -13,17 +13,14 @@ def remove(D, s):
   for r in D: res.update(r - s)
   return res
 
-z2D = defaultdict(set)
-
 data = read('input1.txt')
 
+D = set()
 for on, (x1, x2), (y1, y2), (z1, z2) in tqdm(data):
-  s = Rectangle(x1, y1, x2 + 1, y2 + 1)
-  for z in trange(z1, z2 + 1, leave = False):
-    z2D[z] = add(z2D[z], s) if on else remove(z2D[z], s)
+  s = Cuboid(x1, y1, z1, x2 + 1, y2 + 1, z2 + 1)
+  D = add(D, s) if on else remove(D, s)
 
 num = 0
-for D in z2D.values():
-  for r in D:
-    num += r.area
+for r in D:
+  num += r.volume
 print(num)
